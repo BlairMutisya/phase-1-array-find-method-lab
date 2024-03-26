38fdf96512f16c063d3e416bb549d54d006c625c
Blair <tonnyblair09@gmail.com>
2024-03-26 10:26:41 +0300

    update

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function superbowlWin(superbowlRecords) {
+    // Use the find() method to search for a record with the result "W"
+    const winningRecord = superbowlRecords.find(record => record.result === "W");
+  
+    // If a winning record is found, return the year of the win
+    if (winningRecord) {
+      return winningRecord.year;
+    } else {
+      // If no winning record is found, return undefined
+      return undefined;
+    }
+  }
+  
